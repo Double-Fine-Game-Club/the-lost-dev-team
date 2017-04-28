3c14509529faa322da91e5a2fbe678a26ce745ac
JenniBee <mcjennibee@gmail.com>
2017-04-28 02:09:15 -0400

    Added a menu, a credits screen, and an options screen and connected Anemone's music

--- Assets/scripts/btnToLevel.gd
+++ Assets/scripts/btnToLevel.gd
@@ -1,22 +1,26 @@
-extends Button
-# I will make this button change scenes
+var current_scene
 
-# You can export variables to the Inspector for easy access.
-export var scene = 'res://Levels/Level1.xml'
+func play_pressed():
+	current_scene.queue_free()
+	get_tree().change_scene('res://Levels/Level1.xml')
+
+func play_credits():
+	current_scene.queue_free()
+	get_tree().change_scene('res://Levels/Credits.xml')
+
+func play_options():
+	current_scene.queue_free()
+	get_tree().change_scene('res://Levels/Options.xml')
+
+func quit_pressed():
+	current_scene.queue_free()
+	get_tree().quit();
 
 func _ready():
-	set_process(true)
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() -1)
 
-
-func _process(delta):
-	# buttons have an is_pressed() function to check if it is pressed or not. It returns true of false (a boolean)
-	if is_pressed():
-		# the get_tree() function gets the highest parent of the scene. We can use the change_scene() method to change scenes
-		# The highest parent is the viewport
-		get_tree().change_scene(scene)
-
-# SUMMARY:
-	# Export variables
-	# Move up node hierarchy
-	# Change scenes
-	
+	get_node("play").connect("pressed", self, "play_pressed")
+	get_node("credits").connect("pressed", self, "play_credits")
+	get_node("options").connect("pressed", self, "play_options")
+	get_node("quit").connect("pressed", self, "quit_pressed")
---
+++ Assets/scripts/credits.gd
@@ -0,0 +1,22 @@
+# Credits system adapted from Tiny Chopper Raceway 
+# by Josh "Cheeseness" Bush
+#
+# github.com/Cheeseness/tiny-chopper-raceway
+
+var current_scene
+
+func GoBack_pressed():
+		current_scene.queue_free()
+		get_tree().change_scene('res://Levels/Menu.xml')
+		
+func _ready():
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() -1)
+
+	get_node("GoBack").connect("pressed", self, "GoBack_pressed")
+
+	var file = File.new()
+	file.open("res://Assets/scripts/credits.txt", File.READ)
+	while(!file.eof_reached()):
+		get_node("RichTextLabel").add_text(file.get_line())
+		get_node("RichTextLabel").newline()
