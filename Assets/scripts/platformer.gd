4cf528185289fcddd69a441a5b2b70e2cb4d6d6d
JenniBee <mcjennibee@gmail.com>
2017-04-30 17:17:40 -0400

    Beginning the artist tutorial

--- Assets/scripts/platformer.gd
+++ Assets/scripts/platformer.gd
@@ -1,14 +1,17 @@
 extends Node2D
 # By the Tutorial Doctor
 # Sun Oct 18 22:22:10 EDT 2015
+
 var current_level
+
 #------------------------------------------------------------
 # First, make variables for all of the things that will be in your game
+var tutorial1_is_done
 var player
 var player_animation
 var random_jumpingsound
 var player_samples
-var computer
+var paint_sign
 var computer_samples
 var ray
 export var move_speed = 200
@@ -22,6 +25,9 @@ export var height_speed = -1000
 export var next_level = ''
 #------------------------------------------------------------
 
+func get_random_number():
+    randomize()
+    return randi()%4
 
 # The _ready() function is where we will set up everything.
 #------------------------------------------------------------
@@ -45,11 +51,8 @@ func _ready():
 	# If you want the player to have sound effects, you have to store them in a sample player node
 	player_samples = get_node("SamplePlayer")
 	
-	# The computer node is just a duplicate of the Player node.
-	#computer = get_node("Computer")
-	# Give it the same mode so that it doesn't rotate.
-	#computer.set_mode(2)
-	#computer_samples = get_node("SamplePlayer 2")
+	# The paint sign indicates that the painter can paint.
+	paint_sign = get_node("Paint_Sign")
 	
 	# The player's Raycast2D node checks for collision in a certain direction
 	ray = get_node("Player/RayCast2D")
@@ -59,10 +62,7 @@ func _ready():
 
 	# This node is a StaticBody. We will check collisions with it to restart the level.
 	ground = get_node("Ground")
-	
-	# This is a particle system. We will make it emitt when the player collides with the computer.
-	#hearts =  get_node("Computer/Hearts")
-	
+
 	# Timer nodes are used to do things once a certain amount of time has passed. 
 	timer = get_node("Timer")
 	
@@ -99,13 +99,13 @@ func _process(delta):
 				# Set the axis_velocity on the y axis to -1000,
 				player.set_axis_velocity(Vector2(0,height_speed))
 				# And play the "jump" sample sound
-				randomize()
-				random_jumpingsound = rand_range(1,4)
+				
+				random_jumpingsound = get_random_number()
 				if random_jumpingsound == 1:
 					player_samples.play('jump')
 				if random_jumpingsound == 2:
 					player_samples.play('jump2')
-				else:
+				if random_jumpingsound == 3:
 					player_samples.play('jump3')
 	
 	# Also, if the RayCast2D is colliding with something (if it is on_ground()...
@@ -120,28 +120,16 @@ func _process(delta):
 			# Set the axis_velocity on the x axis the speed variable.
 			player.set_axis_velocity(Vector2(move_speed,0))
 			#player_sprite.set('flip_h',false)
-	
-	
-	# If the computer RigidBody node is in the player's colliding_bodies (it is an array)...
-	#if computer in player.get_colliding_bodies():
-		# And if kissable is equal to true (this will allow us to turn off the kiss sound later)...
-		#if kissable:
-			# Print something,
-			#print('Hey boxy girl!')
-			# Play the "kiss" sample of the computer sample's SamplePlayer node,
-			#computer_samples.play('kiss1')
-			# Also play the "femyawn_ed" sample,
-			#computer_samples.play('femyawn_ed')
-			# And set the hearts paricle system's config/emitting property to true,
-			#hearts.set('config/emitting',true)
-			# Make kissalbe = false as well (this will disable this whole conditional)
-			#kissable = false
-			# Lastly, start the timer (It is set to 3 seconds) and will count down from that,
-			#timer.start()
-			# And connect the "timeout" signal of the timer the next_level() function.
-			#timer.connect('timeout',self,'next_level')
-			# Whew!!!
-	
+		
+	# If the artist is next to the Paint Sign
+	if(paint_sign in player.get_colliding_bodies() && tutorial1_is_done != true):
+		get_node("Artist_Tutorial").show()
+		get_tree().set_pause(true)
+		if Input.is_action_pressed('enter'):
+			get_tree().set_pause(false)
+			get_node('Artist_Tutorial').hide()
+			tutorial1_is_done=true
+
 	# If the ground is part of the player's colliding bodies...
 	if ground in player.get_colliding_bodies():
 		# reload the current scene
