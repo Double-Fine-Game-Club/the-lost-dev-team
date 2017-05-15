75ebcddfd803421a2f30a0f92d487d41f792e043
JenniBee <mcjennibee@gmail.com>
2017-05-14 20:54:33 -0400

    Cleaning out code from platformer.gd that's now in tutorial.gd

--- Assets/scripts/platformer.gd
+++ Assets/scripts/platformer.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends "res://Assets/scripts/global.gd"
 # By the Tutorial Doctor
 # Sun Oct 18 22:22:10 EDT 2015
 
@@ -11,20 +11,8 @@ var exitdoor
 var artistdone
 var coderdone
 var body
-var paintbody
 var laptopbody
 var hornbody
-var tutorial1_is_done
-var tutorial2_is_done
-var tutorial3_begin
-var tutorial3_is_done
-var tutorial4_begin
-var tutorial4_is_done
-var can_blow_on_fan
-var player
-var artist
-var coder
-var musician
 var player_animation
 var random_jumpingsound
 var player_samples
@@ -32,31 +20,37 @@ var paint_sign
 var laptop_sign
 var exit_door
 var horn_sign
-var ray
-var ray_artist
-var ray_coder
-var ray_musician
-export var move_speed = 200
 var ground
 var water
-var timer
-var player_sprite
-export var height_speed = -1000
-# This variable is exported so we can change it for each level
-export var next_level = ''
+var ray
+
+const GRAVITY = 1200.0
+
+#Angle in degrees towards either side that the player can 
+#consider "floor".
+const FLOOR_ANGLE_TOLERANCE = 40
+const WALK_FORCE = 900
+const WALK_MIN_SPEED=400
+const WALK_MAX_SPEED = 700
+const STOP_FORCE = 1300
+const JUMP_SPEED = 600
+const JUMP_MAX_AIRBORNE_TIME=1
+
+var velocity = Vector2()
+var on_air_time=100
+var jumping=false
+
+var prev_jump_pressed=false
 #------------------------------------------------------------
 
 func get_random_number():
     randomize()
     return randi()%4
 
-func _on_Paint_Sign_body_enter(body):
-	paintbody = body.get_name()
-
-func _on_Laptop_Sign_body_enter(body):
+func _on_Laptop_Sign_Artist_body_enter(body):
 	laptopbody = body.get_name()
 
-func _on_Horn_Sign_body_enter(body):
+func _on_Horn_Sign_Coder_body_enter(body):
 	hornbody = body.get_name()
 
 func _on_Exit_Door_body_enter( body ):
@@ -73,28 +67,16 @@ func _on_Ground_body_enter( body ):
 func _ready():
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() -1)
-
-	# Make sure to always enable the _process() function. This is your game loop.
-	set_process(true)
 	current_level = get_tree().get_current_scene().get_filename()
-	# Now we set nodes to those variables we created:
-	# Get the player node. It is a RigidBody node.
-	# The player node should have Sprite,CollisionShape2D, and RayCast2D nodes as children
+
 	artist = get_node("Artist")
 	coder = get_node("Coder")
 	musician = get_node("Musician")
-		
+
 	player = artist
-	# Get the player's animation node. It is an AnimationPlayer node
-	player_animation = get_node("AnimationPlayer")
-	# Make the player able to monitor collision
-	player.set_contact_monitor(true)
-	# Also you have to make sure all collisions are reported
-	player.set_max_contacts_reported(true)
-	# The two steps above could be eliminated, but you need them both to do collision detection for Rigidbodies
-	# Setting the mode of the player rigidbody to 2 makes it not rotate unecessarily 
-	player.set_mode(2)
-	# If you want the player to have sound effects, you have to store them in a sample player node
+	
+	set_process(true)
+	
 	player_samples = get_node("SamplePlayer")
 	
 	# The laptop sign indicates that the coder can hack.
@@ -103,37 +85,7 @@ func _ready():
 	# The horn sign indicates that the musician can blow the horn.
 	horn_sign = get_node("Horn_Sign")
 
-				# The player's Raycast2D node checks for collision in a certain direction
-	ray_artist = get_node("Artist/RayCast2D")
-	# So that the RayCast doesn't detect collisions with the player we need to make the player an exception.
-	# Now the ray will not detect collision with the player node.
-	ray_artist.add_exception(artist)
-
-			# The player's Raycast2D node checks for collision in a certain direction
-	ray_coder = get_node("Coder/RayCast2D")
-	# So that the RayCast doesn't detect collisions with the player we need to make the player an exception.
-	# Now the ray will not detect collision with the player node.
-	ray_coder.add_exception(coder)
-
-			# The player's Raycast2D node checks for collision in a certain direction
-	ray_musician = get_node("Musician/RayCast2D")
-	# So that the RayCast doesn't detect collisions with the player we need to make the player an exception.
-	# Now the ray will not detect collision with the player node.
-	ray_musician.add_exception(musician)
-	
-	ray = ray_artist
-		
-	# This node is a StaticBody. We will check collisions with it to restart the level.
-	exit_door = get_node("Tower/Exit Door")
-	
-	# Timer nodes are used to do things once a certain amount of time has passed. 
-	timer = get_node("Timer")
-	
-	# Player Sprite
-	player_sprite = get_node("Artist/Sprite")
-
-#------------------------------------------------------------
-
+	ray = ("Artist/RayCast2D")
 
 # Custom functions make life easier sometimes
 #------------------------------------------------------------
@@ -141,9 +93,6 @@ func _ready():
 func on_ground():
 	return ray.is_colliding()
 
-# This function goes to the next level. You can use the connect() function to connect a signal to this function
-func next_level():
-	get_tree().change_scene(next_level)
 # END CUSTOM FUNCTIONS
 #------------------------------------------------------------
 
@@ -152,119 +101,111 @@ func next_level():
 #------------------------------------------------------------
 func _process(delta):
 	# If the RayCast2D is colliding with something (if it is on_ground()...
+	#create forces
+	var force = Vector2(0,GRAVITY)
 
+	var stop = velocity.x!=0.0
+	
+	var walk_left = Input.is_action_pressed("ui_left")
+	var walk_right = Input.is_action_pressed("ui_right")
+	var jump = Input.is_action_pressed("ui_up")
 
-
-	if on_ground():
-		# And if the "up" action is pressed...
-		if Input.is_action_pressed("ui_up"):
-			# And if the player_animation AnimationPlayer is not playing (already)...
-			if not player_animation.is_playing():
-				# Then play the "squash" animation,
-				#player_animation.play('squash') #not created yet
-				# Set the axis_velocity on the y axis to -1000,
-				player.set_axis_velocity(Vector2(0,height_speed))
-				# And play the "jump" sample sound
-				if(player == artist):
-					random_jumpingsound = get_random_number()
-					if random_jumpingsound == 1:
-						player_samples.play('jump-jenni')
-					if random_jumpingsound == 2:
-						player_samples.play('jump2-jenni')
-					if random_jumpingsound == 3:
-						player_samples.play('jump3-jenni')
-				else:
-					player_samples.play('grunt-kyle3wynn')
-
-	# Also, if the RayCast2D is colliding with something (if it is on_ground()...
-	if on_ground():
-		# And if the "left" action is pressed...
-		if Input.is_action_pressed('ui_left'):
-			# Set the axis_velocity on the x axis to minus the speed variable.
-			player.set_axis_velocity(Vector2(-move_speed,0))
-			#player_sprite.set('flip_h',true)
-		# And if the "right" action is pressed...
-		if Input.is_action_pressed('ui_right'):
-			# Set the axis_velocity on the x axis the speed variable.
-			player.set_axis_velocity(Vector2(move_speed,0))
-			#player_sprite.set('flip_h',false)
+	var stop=true
+	
+	if (walk_left):
+		if (velocity.x<=WALK_MIN_SPEED and velocity.x > -WALK_MAX_SPEED):
+			force.x-=WALK_FORCE			
+			stop=false
 		
-	#Artist Tutorial
-	if(paintbody == "Artist" && tutorial1_is_done != true):
-		get_node("Artist_Tutorial").show()
-		get_tree().set_pause(true)
-	if Input.is_action_pressed('enter'):
-		get_tree().set_pause(false)
-		get_node('Artist_Tutorial').hide()
-		tutorial1_is_done=true
-	if(paintbody == "Artist" && Input.is_action_pressed('ctrl')):
-		player_samples.play('paint-kyle3wynn')
-		get_node("Platforms/Painted_Platform").show()
-		get_node("Platforms/Painted_Platform/CollisionShape2D").set_trigger(false)
-		#Coder Tutorial Section
-	if(laptopbody == "Artist" && tutorial2_is_done != true):
-		get_node("Coder_Tutorial").show()
-		get_tree().set_pause(true)
-		tutorial2_is_done=true
-	if(laptopbody == "Artist" && tutorial2_is_done == true):
-		if Input.is_action_pressed('enter'):
-			get_tree().set_pause(false)
-			get_node('Coder_Tutorial').hide()
+	elif (walk_right):
+		if (velocity.x>=-WALK_MIN_SPEED and velocity.x < WALK_MAX_SPEED):
+			force.x+=WALK_FORCE
+			stop=false
+	
+	if (stop):
+		var vsign = sign(velocity.x)
+		var vlen = abs(velocity.x)
+		
+		vlen -= STOP_FORCE * delta
+		if (vlen<0):
+			vlen=0
+			
+		velocity.x=vlen*vsign
+		
+	#integrate forces to velocity
+	velocity += force * delta
+	
+	#integrate velocity into motion and move
+	var motion = velocity * delta
+
+	#move and consume motion
+	motion = player.move(motion)
+
+
+	var floor_velocity=Vector2()
+
+	if (player.is_colliding()):
+		# you can check which tile was collision against with this
+		# print(get_collider_metadata())
+
+		#ran against something, is it the floor? get normal
+		var n = player.get_collision_normal()
+
+		if ( rad2deg(acos(n.dot( Vector2(0,-1)))) < FLOOR_ANGLE_TOLERANCE ):
+			#if angle to the "up" vectors is < angle tolerance
+			#char is on floor
+			on_air_time=0
+			floor_velocity=player.get_collider_velocity()
+			#velocity.y=0 
+			
+		#But we were moving and our motion was interrupted, 
+		#so try to complete the motion by "sliding"
+		#by the normal
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
+		
+		#then move again
+		player.move(motion)
+
+	if (floor_velocity!=Vector2()):
+		#if floor moves, move with floor
+		player.move(floor_velocity*delta)
+
+	if (jumping and velocity.y>0):
+		jumping=false
+		
+	if (on_air_time<JUMP_MAX_AIRBORNE_TIME and jump and not prev_jump_pressed and not jumping):	
+		if(player == artist):
+			random_jumpingsound = get_random_number()
+			if random_jumpingsound == 1:
+				player_samples.play('jump-jenni')
+			if random_jumpingsound == 2:
+				player_samples.play('jump2-jenni')
+			else:
+				player_samples.play('jump3-jenni')
+		else:
+			player_samples.play('grunt-kyle3wynn')
+
+		velocity.y=-JUMP_SPEED	
+		jumping=true
+		
+	on_air_time+=delta
+	prev_jump_pressed=jump	
+		
 	if(laptopbody == "Artist" && Input.is_action_pressed('switch') && artistdone != true):
-		get_node("Coder/CollisionShape2D").set_trigger(false)
-		player = coder
-		ray = ray_coder
-		player_sprite = get_node("Coder/Sprite")
-		player.set_max_contacts_reported(true)
-		player.set_mode(2)
-		player.set_contact_monitor(true)
+		player = get_node("Coder")
+		ray = ("Coder/RayCast2D")
+		get_node("Coder/Camera2D").make_current()
+		get_node("Coder/RayCast2D").is_enabled()
 		get_node("Artist").queue_free()
 		artistdone = true
-
-	#Coder Tutorial
-	if(laptopbody == "Coder" && tutorial3_begin != true):
-		get_node("Coder_Tutorial2").show()
-		get_tree().set_pause(true)
-		if Input.is_action_pressed('enter'):
-			get_tree().set_pause(false)
-			get_node('Coder_Tutorial2').hide()
-			tutorial3_begin = true
-	if(laptopbody == "Coder" && Input.is_action_pressed('ctrl')):
-		player_samples.play('code-kyle3wynn')
-		get_node("Tower/closed_elevator").hide()
-		get_node("Moving platform").show()
-		get_node("Tower/CollisionShape2D").set_trigger(true)
-	if(hornbody == "Coder" && tutorial3_is_done != true):
-		get_node("Musician_Tutorial").show()
-		get_tree().set_pause(true)
-		tutorial3_is_done=true
-	if(hornbody == "Coder" && Input.is_action_pressed('enter')):
-		get_tree().set_pause(false)
-		get_node('Musician_Tutorial').hide()
 	if(hornbody == "Coder" && Input.is_action_pressed('switch') && coderdone != true):
-		get_node("Coder/CollisionShape2D").set_trigger(false)
-		player = musician
-		ray = ray_musician
-		player_sprite = get_node("Musician/Sprite")
-		player.set_max_contacts_reported(true)
-		player.set_mode(2)
-		player.set_contact_monitor(true)
+		player = get_node("Musician")
+		ray = ("Musician/RayCast2D")
+		get_node("Musician/Camera2D").make_current()
+		get_node("Musician/RayCast2D").is_enabled()
 		get_node("Coder").queue_free()
 		coderdone = true
-
-	#Musician Tutorial
-	if(hornbody == "Musician" && tutorial4_begin != true):
-		get_node("Musician_Tutorial2").show()
-		get_tree().set_pause(true)
-		if Input.is_action_pressed('enter'):
-			get_tree().set_pause(false)
-			get_node('Musician_Tutorial2').hide()
-			tutorial4_begin = true
-	if(hornbody == "Musician" && Input.is_action_pressed('ctrl')):
-		player_samples.play('horn-kyle3wynn')
-		get_node("Tower/lower_elevator").hide()
-		get_node("Moving platform2").show()
-		get_node("Tower/CollisionShape2D2").set_trigger(true)
 	if(exitdoor == "Musician"):
 		current_scene.queue_free()
 		get_tree().change_scene('res://Levels/Level_end.tscn')
